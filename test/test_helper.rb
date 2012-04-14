3619252e1aae9376f8377234b15a70dc5461343a
Panda Bear <your@email.here>
2012-04-14 18:57:55 +0300

    Added comment regarding test_helper's teardown logic.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,8 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
+  # Source: http://stuartmackey.tumblr.com/post/18545015254/unit-testing-with-mongoid
+  # A logic to clean up the test db's collections.
   teardown :clean_mongodb
   def clean_mongodb
     Mongoid.database.collections.each do |collection|

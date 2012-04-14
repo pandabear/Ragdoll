e208241bf0ba39c744427c1a8a1323b2f7d92388
Panda Bear <your@email.here>
2012-04-14 17:40:56 +0300

    Trying out MongoDB with Mongoid.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ragdoll::Application.config.secret_token = '5ea41c7781d8902d9baafbb69db2665c528441e11c5735ee22e3c13edae780d7a746e2929a2dcf8c9e2bfc673e41a656f64c7a063bd2c24a89cae751aea75213'
